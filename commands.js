6fe2fc778c31573ddd15ce6a676ac1da8e667a3d
pritisthasarkar <pritisthasarkar@gmail.com>
2022-03-14 18:19:16 +0530

    created a separate file for storing all commands

---
+++ commands.js
@@ -0,0 +1,11 @@
+
+function help(){
+    console.log(`
+    1) node main.js tree <path>
+    2) node main.js organize <path>
+    3) node main.js help
+    `)
+}
+module.exports={
+    help:help
+}
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //entry point of my command line
 
+const commands=require("./commands");
 let inputArr=process.argv.slice(2);
 let command=inputArr[0];
+let folderPath=inputArr[1];
 switch(command){
     case "tree":
         //call tree function
@@ -10,8 +12,10 @@ switch(command){
         //call organize function
         break;
     case "help":
+        commands.help();
         //call help function
         break;
     default:
         console.log("Invalid Command");
 }
+
