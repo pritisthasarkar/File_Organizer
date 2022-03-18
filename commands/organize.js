52037f04f7c46d44b4070487d6e2a9e1cca877ce
pritisthasarkar <pritisthasarkar@gmail.com>
2022-03-18 11:31:32 +0530

    organize implemnted

--- commands/organize.js
+++ commands/organize.js
@@ -1,43 +1,66 @@
-const fs=require("fs");
-const path=require("path");
+const fs = require("fs");
+const path = require("path");
 
 let types = {
     media: ["mp4", "mkv", "mp3"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
     app: ['exe', 'dmg', 'pkg', "deb"],
-    images: ['png','jpg','jpeg']
+    images: ['png', 'jpg', 'jpeg']
 }
 
-function organize(srcPath){
+function organize(srcPath) {
     //1. <===============to check if srcPath is present=============>
-    if(srcPath==undefined){
+    if (srcPath == undefined) {
         /*The process.cwd() method returns the current working directory of the Node.js process.*/
-        srcPath=process.cwd(); 
+        srcPath = process.cwd();
     }
     //2. <=======to create a path where we will make the organized_files folder======>
-    let organizedFilesPath=path.join(srcPath,"organized_files");
+    let organizedFilesPath = path.join(srcPath, "organized_files");
     //internally path.join() is same as srcPath+"\"+"organized_files";
     //3.<===========to check if the path where we want to create our folder already exists or not====>
-    if(!fs.existsSync(organizedFilesPath)){
+    if (!fs.existsSync(organizedFilesPath)) {
         fs.mkdirSync(organizedFilesPath);
         console.log("directory sucessfully created");
-    }else{
+    } else {
         console.log("directory already exists");
         // return;
     }
-    
+
     //4.<=====to scan the entire srcPath and store all the available files in an array=====>
     //fs.readdirSync(directoryPath) reads the content of a directory and returns an array of all the files that are present inside the directory
-    let allFiles=fs.readdirSync(srcPath);
+    let allFiles = fs.readdirSync(srcPath);
     console.log(allFiles);
 
     //5.<=======to traverse through allFiles and classify them on the basis of their extension======>
-    for(let i=0;i<allFiles.length;i++){
-        // let extension=allFiles[i].substring(allFiles[i].indexOf('.')+1);
-        let extension=path.extname(allFiles[i]);
-        console.log(extension);
+    for (let i = 0; i < allFiles.length; i++) {
+        //1. to check if it is a file or folder. we will proceed forward only if it is a file.
+
+        let fullPathOfFile = path.join(srcPath, allFiles[i]);
+        let isFile = fs.lstatSync(fullPathOfFile).isFile(); //fs.lstatSync() gives information about the link provided(the link can be anything). lstatSync() has a function isFile()/isDirectory() which tells us if the link provided corresponds to a file or a directory. true --> agar file hain false --> agar folder hain.
+        if (isFile) {
+            //2. to get the extension of the file
+            let extension=allFiles[i].split(".")[1];
+            //3. to get the designated folder name where the file belongs
+            let folderName=getFolderName(extension); //archives
+            //4. copy the file from the source folder(srcPath) to the destination folder(folder_name eg:- images,songs etc.).
+            //          copy from    copy what    paste where
+            copyFileToDest(srcPath,fullPathOfFile,folderName);
+        }
+
     }
 }
-let srcPath="C:\\Users\\HP\\Desktop\\fjp dev\\js\\file_organizer\\downloads"
+
+function getFolderName(extension){
+
+    //code
+    return folderName;
+}
+
+function copyFileToDest(srcPath,fullPathOfFile,folderName){
+    
+    //code
+}
+
+let srcPath = "C:\\Users\\HP\\Desktop\\fjp dev\\js\\file_organizer\\downloads"
 organize(srcPath);
